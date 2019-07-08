5faad863ce0703502cc4dcdaf942423ba3bd8e5f
Shane Zhao <s.x.zhao@gmail.com>
2019-07-07 17:13:20 -0700

    setup mongoose schema

--- database/index.js
+++ database/index.js
@@ -1,2 +1,37 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/recommendations', {useMongoClient: true});
+
+let recSchema = mongoose.Schema({
+  roomType: String,
+  city: String,
+  state: String,
+  country: String,
+  price: Number,
+  reviewCount: Number,
+  ratingStars: Number,
+  about: String,
+  space: String,
+  neighborhood: String,
+  review: {
+    name: String,
+    date: Date,
+    comment: String
+  }
+});
+
+const Rec = mongoose.model('Recommendations', recSchema);
+
+let save = (obj, callback) => {
+  const userRec = new Rec(obj);
+  userRec.save((err) => {
+    if(err) {
+      callback(err);
+    }
+    else {
+      callback(null, 'Saved')
+    }
+  });
+}
+
+module.exports.save = save;
+module.exports.Rec = Rec;
